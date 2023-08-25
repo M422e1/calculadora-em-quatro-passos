e2d78a001bc95a56c411aa4f73cb2f5e1ab71081
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 19:31:27 -0300

    first commit

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+print("---------------------------------------------")
+print("----------------CALCULADORA------------------")
+print("---------------------------------------------")
+
+s1=int(input("Digite o primeiro valor......: "))
+s2=int(input("Digite o segundo valor.......: "))
+
+soma=s1+s2
+
+print("\nSoma dos valores: ",soma)
