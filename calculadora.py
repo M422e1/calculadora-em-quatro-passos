4b1818284d67651dcd7b9e0046a18873a2a1427d
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 21:34:40 -0300

    Calcula a função geral

--- calculadora.py
+++ calculadora.py
@@ -6,5 +6,13 @@ s1=int(input("Digite o primeiro valor......: "))
 s2=int(input("Digite o segundo valor.......: "))
 
 soma=s1+s2
-
 print("\nSoma dos valores: ",soma)
+
+subtração=s1-s2
+print("\nDiferença entre os valores: ",subtração)
+
+multiplicação=s1*s2
+print("\nMultiplicação entre os valores: ",multiplicação)
+
+divisor=s1/s2
+print("\nDivisão entre os valores: ",divisor)
